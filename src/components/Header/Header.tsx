26fd0d311c747370cd25b208195737bf69a7344a
luccas costa <lucaspcosta70@gmail.com>
2024-04-21 13:26:10 -0300

    quarto dia parte2

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
 
   return (
     <header className='bg-green2 sticky top-0 z-10'>
-      <div className='flex items-center justify-between mr-14 ml-14 p-3'>
+      <div className='flex items-center justify-between 2xl:mr-14 2xl:ml-14 xl:mr-10 xl:ml-10 md:mr-6 md:ml-6 sm:mr-2 sm:ml-2 mr-0 ml-0 p-3'>
         <div>
           <Image
             src='/logo-transparente.png'
--- src/components/Main/Section1/Section1.tsx
+++ src/components/Main/Section1/Section1.tsx
@@ -3,7 +3,7 @@ import Imagem from "../../../../public/wppmato.jpg";
 
 export default function Section1() {
   return (
-    <div className='bg-green2 pl-16 pr-16 pb-16 pt-2 z-0'>
+    <div className='bg-green2 2xl:pl-16 xl:pl-12 md:pl-8 sm:pl-4 pl-2 2xl:pr-16 xl:pr-12 md:pr-8 sm:pr-4 pr-2 2xl:pb-16 xl:pb-12 md:pb-8 sm:pb-4 pb-2 pt-2 z-0'>
       <div
         className='2xl:w-[93vw] 2xl:h-[80vh] xl:h-[90vh] lg:h-[90vh] md:h-[90vh] sm:h-[90vh] h-[90vh] relative'
         style={{
@@ -11,10 +11,10 @@ export default function Section1() {
           backgroundSize: "cover",
         }}
       >
-        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-pink1 text-3xl xl:text-7xl lg:text-5xl md:text-5xl sm:text-4xl text-center font-bold flex flex-col'>
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-pink1 text-4xl xl:text-7xl lg:text-6xl md:text-6xl sm:text-5xl text-center font-bold flex flex-col'>
           Venha conhecer sua TekoBag`s
           <div className='mt-4'>
-            <button className='text-sm xl:text-3xl lg:text-2xl md:text-xl sm:text-lg border-2 border-pink1 px-4 py-2 hover:bg-pink1 hover:text-green2'>
+            <button className='text-sm xl:text-4xl lg:text-4xl md:text-3xl sm:text3xl border-2 border-pink1 px-4 py-2 hover:bg-pink1 hover:text-green2'>
               Contato
             </button>
           </div>
