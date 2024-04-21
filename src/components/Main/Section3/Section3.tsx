4b0a5c912b30fd6f2a6e72915961c6b74f6e15ba
luccas costa <lucaspcosta70@gmail.com>
2024-04-21 16:03:19 -0300

    quarto dia part8

--- src/components/Main/Section3/Section3.tsx
+++ src/components/Main/Section3/Section3.tsx
@@ -12,10 +12,13 @@ export default function Section3() {
   const [isHovered, setIsHovered] = useState(false);
   return (
     <div className={styles.degrade}>
-      <div className='flex items-center justify-center gap-x-4 pt-28 pb-28'>
-        <div className='bg-green3 w-[47vw] h-[73vh] xl:w-[36.5vw] rounded-l-lg  flex flex-col items-center 2xl:pt-28 pt-6 text-yellow1 text-2xl xl:text-4xl lg:text-4xl md:text-3xl sm:text-2xl font-extrabold'>
+      <div className='flex flex-wrap items-center justify-center gap-4 pt-28 pb-28'>
+        <div
+          className='bg-green3 w-[90vw] h-[70vh] md:w-[700px] md:h-[700px] rounded-l-lg  flex flex-col items-center 2xl:pt-28 sm:pt-28
+         pt-14 text-yellow1 text-4xl font-extrabold'
+        >
           Sobre nós
-          <div className='text-base sm:text-lg md:text-xl lg:text-2xl text-zinc-800 font-semibold w-3/4 h-3/4 pl-2 pt-2'>
+          <div className='text-xl sm:text-2xl text-zinc-800 font-semibold w-3/4 h-3/4 pl-2 pt-2'>
             Somos um Startup desvolvida sob um projeto escolar, com um time de
             12 pessoas mais 1 mentor. Nosso objetivo é trazer soluções
             relacionadas a Eco Bags, nós da Teko Bag fazemos uma mediação e
@@ -30,7 +33,7 @@ export default function Section3() {
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
-          className='rounded-r-lg w-[47vw] h-[73vh] xl:w-[36.5vw]'
+          className='rounded-r-lg w-[90vw] h-[70vh] md:w-[700px] md:h-[700px]'
         ></div>
       </div>
       <div className='flex flex-col gap-y-4 items-center justify-center'>
