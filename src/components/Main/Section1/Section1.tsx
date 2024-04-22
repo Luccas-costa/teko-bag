99c83408a28acdf188eef483deb285eaf918fc9f
luccas costa <lucaspcosta70@gmail.com>
2024-04-22 11:38:32 -0300

    quinto dia colocando apostofro no treko bags

--- src/components/Main/Section1/Section1.tsx
+++ src/components/Main/Section1/Section1.tsx
@@ -15,7 +15,7 @@ export default function Section1() {
         }}
       >
         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-pink1 text-6xl 2xl:text-7xl xl:text-7xl lg:text-8xl md:text-8xl sm:text-7xl text-center font-bold flex flex-col'>
-          Venha conhecer sua TekoBag`s
+          Venha conhecer sua TekoBag{'\''}s
           <div className='mt-4'>
             <button className='text-3xl 2xl:text-4xl xl:text-4xl lg:text-6xl md:text-5xl sm:text5xl border-2 xl:border-3 border-pink1 px-4 py-2 hover:bg-pink1 hover:text-green2'>
               <a href='https://www.instagram.com/tekobags/' target='_blank' >Contato</a>
