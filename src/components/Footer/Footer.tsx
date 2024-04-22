6ae8de5c5818f731f5b60382978386b44ca39069
luccas costa <lucaspcosta70@gmail.com>
2024-04-22 12:02:59 -0300

    quinto dia teste

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -38,14 +38,7 @@ const [width, setWidth] = useState(window.innerWidth);
       <div className='flex flex-col items-center pt-12 pl-16 pr-16'>
         <div className='flex gap-x-6 pb-12'>
           <div className='flex items-center justify-center'>
-            {width >= 560 &&
-              <Image
-                src='/logo-transparente.png'
-                width={125}
-                height={125}
-                alt='logo'
-              />
-            }
+            {width >= 560 && <Image src='/logo-transparente.png' width={125} height={125} alt='logo' /> }
           </div>
           <hr className='w-1 md:h-48 sm:h-52 h-[32vh] bg-zinc-600' />
           <div className='text-zinc-600 font-semibold flex flex-col flex-nowrap items-start'>
