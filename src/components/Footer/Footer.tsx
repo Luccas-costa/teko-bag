18948f99e65970068306de76c37a2889dc9c9c81
luccas costa <lucaspcosta70@gmail.com>
2024-04-22 16:25:50 -0300

    quinto dia parte manha

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -23,7 +23,7 @@ export default function Footer() {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 560);
+      setIsMobile(window.innerWidth > 560);
     };
 
     handleResize(); // Chamar a função inicialmente para definir o estado
@@ -31,13 +31,20 @@ export default function Footer() {
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-  
+
   return (
     <div className='flex flex-col' id='footer'>
       <div className='flex flex-col items-center pt-12 pl-16 pr-16'>
         <div className='flex gap-x-6 pb-12'>
           <div className='flex items-center justify-center'>
-            {isMobile && <Image src='/logo-transparente.png' width={125} height={125} alt='logo' /> }
+            {isMobile && (
+              <Image
+                src='/logo-transparente.png'
+                width={125}
+                height={125}
+                alt='logo'
+              />
+            )}
           </div>
           <hr className='w-1 md:h-48 sm:h-52 h-[32vh] bg-zinc-600' />
           <div className='text-zinc-600 font-semibold flex flex-col flex-nowrap items-start'>
