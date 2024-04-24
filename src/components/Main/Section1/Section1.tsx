38d6c49b5fedfaf880fc715ac97e6b75bcfb4c88
luccas costa <lucaspcosta70@gmail.com>
2024-04-24 18:04:23 -0300

    setimo dia

--- src/components/Main/Section1/Section1.tsx
+++ src/components/Main/Section1/Section1.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Imagem from "../../../../public/wppmato2.jpg";
+import Imagem from "../../../../public/wppmato3.png";
 import styles from "./index.module.css";
 
 export default function Section1() {
@@ -16,10 +16,12 @@ export default function Section1() {
         }}
       >
         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl 2xl:text-7xl xl:text-7xl lg:text-8xl md:text-8xl sm:text-7xl text-center font-bold flex flex-col'>
-          Venha conhecer sua TekoBag{'\''}s
+          Venha conhecer sua TekoBag{"'"}s
           <div className='mt-4'>
-            <button className='text-3xl 2xl:text-4xl xl:text-4xl lg:text-6xl md:text-5xl sm:text5xl border-2 xl:border-3 border-pink1 px-4 py-2 hover:bg-pink1 hover:text-green2'>
-              <a href='https://www.instagram.com/tekobags/' target='_blank' >Contato</a>
+            <button className='text-3xl 2xl:text-4xl xl:text-4xl lg:text-6xl md:text-5xl sm:text5xl border-2 xl:border-3 border-white px-4 py-2 hover:bg-white hover:text-green2'>
+              <a href='https://www.instagram.com/tekobags/' target='_blank'>
+                Contato
+              </a>
             </button>
           </div>
         </div>
--- src/components/Main/Section2/Section2.tsx
+++ src/components/Main/Section2/Section2.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import Cards from "./Cards/Cards";
 
 export default function Section2() {
