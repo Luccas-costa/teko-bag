a45cb7187d2bd58fdf408871117a0ddcaaccb2e7
luccas costa <lucaspcosta70@gmail.com>
2024-04-24 18:13:19 -0300

    setimo dia

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,39 @@
 "use client";
-import React from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
-import Logo from "./Logo/Logo";
 import Time from "./Time/Time";
 import Contatos from "./Contatos/Contatos";
 import Developed from "./Developed/Developed";
 
 export default function Footer() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth > 560);
+    };
+
+    handleResize(); // Chamar a função inicialmente para definir o estado
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className='flex flex-col' id='footer'>
       <div className='flex flex-col items-center pt-12 pl-16 pr-16'>
-        <div className='flex gap-x-6 pb-12'>
-          <Logo />
+        <div className='flex justify-evenly gap-x-6 pb-12'>
+          <div className='flex items-center justify-center'>
+            {isMobile && (
+              <Image
+                src='/logo-transparente.png'
+                width={125}
+                height={125}
+                alt='logo'
+              />
+            )}
+          </div>
 
           <hr className='w-1 md:h-48 sm:h-52 h-[32vh] bg-zinc-600' />
 
