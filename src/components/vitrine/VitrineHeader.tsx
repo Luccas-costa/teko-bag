00214c48274269cff3621df74d1b841c13478e26
luccas costa <lucaspcosta70@gmail.com>
2024-05-19 17:33:06 -0300

    ajustes novo

--- src/components/vitrine/VitrineHeader.tsx
+++ src/components/vitrine/VitrineHeader.tsx
@@ -23,17 +23,14 @@ export default function VitrineHeader() {
 
   return (
     <header className='w-full py-3 flex justify-between items-center px-2 space-x-2'>
-      <div className='rounded-xl 2xl:w-[88%] lg:w-[82%] md2:w-[80%] sm1:w-[76%] xm1:w-[72%] xm2:w-[68%] sm2:w-[65%] xm6:w-[62%] w-[59%] bg-white/15 xm5:py-[0.6rem] xm7:py-[0.9rem] py-[0.9rem] px-3 shadow-lg'>
-        <ul className='font-semibold text-xs xm6:text-sm sm2:text-base xm3:text-lg xm1:text-xl text-white flex justify-between'>
+      <div className='rounded-xl 2xl:w-[88%] lg:w-[82%] md2:w-[80%] sm1:w-[72%] xm1:w-[70%] xm2:w-[65%] sm2:w-[65%] xm6:w-[62%] w-[59%] bg-white/15 xm5:py-[0.6rem] xm7:py-[0.6rem] py-[0.9rem] px-3 shadow-lg'>
+        <ul className='font-semibold text-base xm6:text-lg sm2:text-lg xm3:text-lg xm1:text-xl text-white flex justify-between'>
           <li className='md:pl-4'>
             {firstName ? `Bem-vindo, ${firstName}` : "Carregando..."}
           </li>
-          <li className='border-b border-r border-black px-1 active:border-none'>
-            <Link href='/'>home</Link>
-          </li>
         </ul>
       </div>
-      <div className='rounded-xl 2xl:w-[12%] lg:w-[18%] md2:w-[20%] sm1:w-[24%] xm1:w-[28%] xm2:w-[32%] sm2:w-[35%] xm6:w-[38%] w-[41%] bg-white/15 py-1 px-3 shadow-lg'>
+      <div className='rounded-xl 2xl:w-[12%] lg:w-[18%] md2:w-[20%] sm1:w-[28%] xm1:w-[30%] xm2:w-[35%] sm2:w-[35%] xm6:w-[38%] w-[41%] bg-white/15 py-1 px-3 shadow-lg'>
         <ul className='flex justify-between items-center'>
           <li>
             <SignedIn>
