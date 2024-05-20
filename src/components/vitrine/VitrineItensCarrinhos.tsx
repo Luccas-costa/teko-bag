e5970562a03ead25f5b90fcac4cdbffa23c8cf6f
luccas costa <lucaspcosta70@gmail.com>
2024-05-20 18:50:59 -0300

    terminando ajustes de texto

--- src/components/vitrine/VitrineItensCarrinhos.tsx
+++ src/components/vitrine/VitrineItensCarrinhos.tsx
@@ -7,7 +7,7 @@ import { Trash } from "@phosphor-icons/react/dist/ssr";
 export default function VitrineItensCarrinhos() {
   return (
     <div className='w-90% border border-zinc-950 py-2 flex space-x-2 items-center relative'>
-      <div>
+      <div className='w-[30%]'>
         <Image
           src='/skeleton.png'
           width={100}
@@ -16,12 +16,16 @@ export default function VitrineItensCarrinhos() {
           className='border border-zinc-950 ml-4'
         />
       </div>
-      <div className='flex flex-col items-end w-full mr-2 pr-4'>
-        <div className='text-lg menuxm2:text-xl font-semibold'>produtos</div>
-        <div className='text-sm menuxm2:text-md font-semibold'>
-          quantidade produtos
+      <div className='flex flex-col items-end w-[70%] mr-2 pr-4 '>
+        <div className='text-lg menuxm2:text-xl font-semibold truncate w-[90%] text-end'>
+          produtos
         </div>
-        <div className='text-md menuxm2:text-lg font-bold pr-2'>R$ 89,99</div>
+        <div className='text-sm menuxm2:text-md font-semibold truncate w-[90%] text-end'>
+          quantidade produtos aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        </div>
+        <div className='text-md menuxm2:text-lg font-bold pr-2 truncate w-[90%] text-end'>
+          R$ 89,99
+        </div>
       </div>
       <div className='absolute right-4 top-[-17px] w-24 h-[1.8rem] bg-green-600 border border-zinc-950 flex items-center justify-evenly'>
         <button>
