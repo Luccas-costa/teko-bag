f122916333499b769b13b120f4732a9bfd2d039e
luccas costa <lucaspcosta70@gmail.com>
2024-06-23 16:28:27 -0300

    desativando email pro cliente

--- src/app/api/email-compra-cliente/route.ts
+++ src/app/api/email-compra-cliente/route.ts
@@ -2,19 +2,19 @@
 
 import { TekoBagCompraEmailCliente } from "@/email/teko-bag-compra-cliente";
 import { NextRequest, NextResponse } from "next/server";
-import { useUserEmail } from "@/hooks/useUserEmail";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.EMAIL_API_KEY);
 
 export async function POST(req: NextRequest) {
-  const userEmail = useUserEmail();
+  // const userEmail = useUserEmail(); depois achar um jeito de mandar o email do comprador via o body pois desta maneira nao funciona
+  // const userEmail = useUserEmail(); depois achar um jeito de mandar o email do comprador via o body pois desta maneira nao funciona
   try {
     const { subtitulo, email, instagram, nome } = await req.json();
 
     const data = await resend.emails.send({
       from: `${process.env.EMAIL_FROM_NAME} <${process.env.EMAIL_FROM_EMAIL}>`,
-      to: [`${userEmail}`],
+      to: ["tekobag@gmail.com"],
       subject: "Obrigado por comprar na teko bag!",
       react: TekoBagCompraEmailCliente({
         subtitulo,
