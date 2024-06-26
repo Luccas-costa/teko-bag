35962a4abf62f3ab1ebb3cdb25283ddf6b688581
luccas costa <lucaspcosta70@gmail.com>
2024-06-26 18:38:01 -0300

    terminando button voltar ao topo home

--- src/components/home/BotaoVoltarAoTopo.tsx
+++ src/components/home/BotaoVoltarAoTopo.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { ArrowUp } from "@phosphor-icons/react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function BotaoVoltarAoTopo() {
+  const [isVisible, setIsVisible] = useState(false);
+
   const scrollToTop = (): void => {
     window.scrollTo({
       top: 0,
@@ -11,15 +13,32 @@ export default function BotaoVoltarAoTopo() {
     });
   };
 
+  const handleScroll = () => {
+    if (window.scrollY > 200) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className='fixed bottom-5 right-5'>
-      <div
-        className='w-8 h-8 rounded-full bg-zinc-200/20 shadow-lg flex items-center justify-center cursor-pointer'
-        onClick={scrollToTop}
-        role='button'
-      >
-        <ArrowUp color='#fff' size={25} />
-      </div>
+      {isVisible && (
+        <div
+          className='w-8 h-8 rounded-full bg-zinc-200/20 shadow-lg flex items-center justify-center cursor-pointer'
+          onClick={scrollToTop}
+          role='button'
+        >
+          <ArrowUp color='#fff' size={25} />
+        </div>
+      )}
     </div>
   );
 }
