de733b455c501b6300d7a6823e2c2e9691ca2838
luccas costa <lucaspcosta70@gmail.com>
2024-08-09 17:09:52 -0300

    arruando a poha da virgula

--- src/utils/insertBD.ts
+++ src/utils/insertBD.ts
@@ -37,7 +37,7 @@ export async function insertBD({
 }) {
   try {
     console.log("Enviando dados para o banco de dados...");
-    await sql`INSERT INTO ClientesC (id, email, instagram, bairro, rua, complemento, nurmo, cidade, cep, itens, quantidade, datacompra, telefone, atendido, emailverificacao) VALUES (${id}, ${email}, ${instagram}, ${bairro}, ${rua}, ${complemento}, ${nurmo}, ${cidade}, ${cep}, ${itens}, ${quantidade} , ${datacompra}, ${telefone}, ${atendido} ${emailverificacao})`;
+    await sql`INSERT INTO ClientesC (id, email, instagram, bairro, rua, complemento, nurmo, cidade, cep, itens, quantidade, datacompra, telefone, atendido, emailverificacao) VALUES (${id}, ${email}, ${instagram}, ${bairro}, ${rua}, ${complemento}, ${nurmo}, ${cidade}, ${cep}, ${itens}, ${quantidade} , ${datacompra}, ${telefone}, ${atendido}, ${emailverificacao})`;
     console.log("Enviado com sucesso!");
   } catch (error) {
     console.log(error);
